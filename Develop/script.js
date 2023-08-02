d1be912256b8f0a2d279dd138a27a179b1b595a0
Mona Lisa <waynecharrette@gmail.com>
2023-08-01 21:14:49 -0400

    added logic for color background

--- Develop/script.js
+++ Develop/script.js
@@ -6,11 +6,34 @@ $(function () {
   var today = dayjs();
   $('#currentDay').text(today.format('dddd, MMMM D YYYY'));
 
+  var currentHour = dayjs().format('H');
+
+
+  console.log(currentHour);
+  
+
+  $(".time-block").each(function () {
+    var hour = parseInt($(this).closest('.time-block').attr('id'));
+    console.log(hour);
+    if (currentHour > hour) {
+      $(this).addClass('past')
+    } else if (currentHour === hour) {
+      $(this).removeClass('past')
+      $(this).addClass('present')
+    } else {
+      // (currentHour < hour) {
+      $(this).addClass('future')
+    }
+  });
+
 
   $('.saveBtn').on('click', function () {
     var timeBlockId = $(this).closest('.time-block').attr('id');
+    console.log(timeBlockId);
     var userInput = $(this).siblings('.description').val();
     localStorage.setItem(timeBlockId, userInput);
   })
+
+
 
 });
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 - "initial commit" - updated a small bit of html, reformatted the folder structure slightly.
 - "added the date and committed things to local storage" - really quickly added the date and created a formula to commit text to local storage
+- "added logic for color background" added the jv function for the background colors 
