9ecff6c9f5aa120db9675b91288b13597a18fa8c
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-08-03 19:07:36 -0400

    added images to read me and added comments to code

--- Develop/script.js
+++ Develop/script.js
@@ -3,41 +3,51 @@
 // in the html.
 $(function () {
 
+  //Pulls today's date and reformats it. Adds it to the current day. 
   var today = dayjs();
   $('#currentDay').text(today.format('dddd, MMMM D YYYY'));
 
-  var currentHour = dayjs().format('H');
+  // var currentHour = dayjs().format('H');
+  var currentHour = 13
 
 
   console.log(currentHour);
   
-
+   //Creates a for loop. or each, which will compare Current hour to hour and add the class to .timeblock depending on what the two values are. 
   $(".time-block").each(function () {
+    // sets the variable for hour, converting the value to a number via parseInt
     var hour = parseInt($(this).closest('.time-block').attr('id'));
-    console.log(hour);
+    // console.log(hour);
+    //if Current hour is less than the hour add the class of psat.
     if (currentHour > hour) {
       $(this).addClass('past')
-    } else if (currentHour === hour) {
+      // if current hour is equal to hour, add class of present and remove class of past. 
+    } else if (currentHour == hour) {
       $(this).removeClass('past')
       $(this).addClass('present')
     } else {
-      // (currentHour < hour) {
+      // if current hour is anything else, set the class to future. 
       $(this).addClass('future')
     }
   });
 
-
+   // sets a click button for save icon.
   $('.saveBtn').on('click', function () {
+    // sets a variable for the closest time-block class to the button. 
     var timeBlockId = $(this).closest('.time-block').attr('id');
-    console.log(timeBlockId);
+    // console.log(timeBlockId);
+    // sets the variable for the user input. 
     var userInput = $(this).siblings('.description').val();
+    // logs the userInput to storage. 
     localStorage.setItem(timeBlockId, userInput);
   })
 
   $('.time-block').each(function () {
+    // grabs the timeBlock variable. 
     var timeBlockId = $(this).attr('id');
+    // sets the values we just entered from local storage 
     var storedValue = localStorage.getItem(timeBlockId);
-
+    // if the local storage isn't empty, it pulls the storage and sets description value to that corresponding with that value. 
     if (storedValue !== null) {
       $(this).find('.description').val(storedValue);
     }
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler 
+
+The code was pulled from module five and updated. Outside of the javascript, not too many changes were needed in the html or css file. Unfortunately, I left the js and style sheets in the Develop folder and did not create an original folder for them. I did not need to make as many pushes as I would normally since the project didn't require as much experimentation. 
 
 # Version History
 
@@ -7,3 +9,13 @@
 - "added logic for color background" added the jv function for the background colors 
 - "fixed issues with scheduler formatting" small copy and paste issue with the html caused the scheduler hour margins to be off. 
 - "added the getStorage to jv" - added a getStorage function and added a text-center to the html to center the top text. 
+- "added images to read me and added comments to code" - just cleaned up some code and the readme. 
+
+# Live Deployment
+
+https://waynecharr.github.io/Day_Scheduler/
+
+# Live Images
+
+<img src="Assets/Images/day_scheduler_1.PNG">
+<img src="Assets/Images/day_scheduler_2.PNG">
